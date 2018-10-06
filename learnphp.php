e53ef5e7b93f8bfc30e8a2bd4019927208d91f06
Ian Harwell <sirharwell@gmail.com>
2018-10-06 00:13:50 -0600

    address

--- learnphp.php
+++ learnphp.php
@@ -46,4 +46,66 @@
 				F : Full text for the month
 				j : Day of the month
 				S : Suffix for the day st, nd, rd, etc.
-				Y : 4 digit year
+				Y : 4 digit year			*/
+			echo date('h:i:s:u a, l F jS Y e');
+			echo "</p>";
+
+			/*
+				You store values in variables that have a name
+				that starts with a $
+
+				Variables can be of any length and contain letters,
+				numbers, or underscores
+
+				They can't begin with a digit adnd are case sensitive.
+				numOfCats is not equal to numofcats
+
+				A variable is created and given a data type when it
+				receives a value. That data type can change based on
+				if the data is changed.
+
+				a. Integer : Whole Numbers
+				b. Float : Decimal Numbers
+				c. String : Strings or characters
+				d. Boolean : true or false
+				e. Array : Multiple Items
+				f. Object : A Object defined by a class
+
+				A variable by default gets the value NULL
+
+				You can access values from the html that called
+				this php script to execute by putting the name
+				assigned in the html in single quotes
+
+				The data is stored in an array which is named
+				$_POST
+			*/
+
+			$usersName = $_POST['username'];
+			$streetAddress = $_POST['streetaddress'];
+			$cityAddress = $_POST['cityaddress'];
+
+			echo '<p>Your Information</p>';
+
+			// You can combine variables with text using a .
+
+			echo $usersName. ' lives at </br>';
+			echo $streetAddress. ' in </br>';
+			echo $cityAddress. '</br></br>';
+
+			/*
+				You can define text using heredoc syntax in the
+				same way you use double quotes.
+				Starts with <<< and an identifier that can't be
+				used any place else in the text.
+				It ends with the identifier and a semicolon
+				without any white space or anything else.
+			*/
+			$str = <<<EOD
+			The customers name is
+			$usersName and they
+			live at $streetAddress
+			in $cityAddress</br></br>
+EOD;
+
+			echo $str;
